ef53bea41f09da2dbe1784c2a83bd5578aa209f3
Mandla Mbolekwa <mandlambolekwa@gmail.com>
2026-03-08 16:54:15 +0200

    Updated home UI

--- app/page.js
+++ app/page.js
@@ -1,6 +1,45 @@
+'use client'
+
+import { useState } from "react";
 import FormValidation from "./components/FormValidation";
 
 export default function Home() {
+   const [member, setMember] = useState({
+    email: "",
+    endpoint: "",
+  });
+  const [serverResponse, setServerResponse] = useState({});
+
+  const handleChange = (evt) => {
+    setMember((member) => ({
+      ...member,
+      [evt.target.name]: evt.target.value,
+    }));
+  };
+
+  async function submitHandler(evt) {
+    evt.preventDefault();
+    try {
+      if (member.endpoint === "" || member.email === "") {
+        return setServerResponse("Email and endpoint are required!");
+      } else {
+        
+        const response = await fetch("/api/validate", {
+           method: "POST",
+           headers: {
+             "Content-Type": "application/json",
+           },
+           body: JSON.stringify(member),
+         });
+
+         setServerResponse(await response.json());
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
 
       <main className="flex min-h-screen w-full flex-col sm:px-6 py-12 lg:px-8 lg:items-center sm:items-start">
@@ -14,11 +53,13 @@ export default function Home() {
           <p className="text-3xl font-bold tracking-widest sm:text-4xl ">
             API testing <span className="text-sky-900">Hub</span>
           </p>
-          <p className="text-muted-foreground text-sm leading-8 text-gray-600 px-10 sm:px-0 sm:text-lg">
-            The go to tool for validating and sorting your strings.
-          </p>
         </div>
-        <FormValidation />
+        <FormValidation
+          member={member}
+          handleChange={handleChange}
+          submitHandler={submitHandler}
+          serverResponse={serverResponse}
+         />
       </main>
   );
 }
